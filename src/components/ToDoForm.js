8863dd2dcfd9a54478587ecd3875bb41431f509d
AD <atar.dci@gmail.com>
2019-05-29 15:17:30 +0200

    Add ref to input field

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 export default class ToDoForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    textInput = React.createRef();
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(`Create new item: ${this.textInput.current.value}`);
+    }
+
     render() {
         return (
-            <form className="input-group my-3">
+            <form className="input-group my-3" onSubmit={this.handleSubmit}>
                 <input
                     className="form-control"
                     type="text"
                     placeholder="Add a new to-do item ..."
+                    ref={this.textInput}
                 />
                 <div className="input-group-append">
                     <button className="btn btn-outline-secondary" type="submit">
