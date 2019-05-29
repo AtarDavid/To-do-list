466d921f269c577cdd6a46a32bcf8e9e4e8c39cf
AD <atar.dci@gmail.com>
2019-05-29 16:20:41 +0200

    Render list conditionally

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,17 +3,19 @@ import ToDoItem from "./ToDoItem";
 
 export default class ToDoList extends Component {
     render() {
-        return (this.props.items ?
-            <div className="todo-list">
-                <ul className="todo-items">
-                    {Object.keys(this.props.items).map(uuid => (
-                        <ToDoItem key={`todo-item-${uuid}`} data={this.props.items[uuid]} />
-                    ))}
-                </ul>
-            </div>
-            :
+        console.log(Object.keys(this.props.items).length)
+        return (
+            (Object.keys(this.props.items).length > 0 &&
+                <div className="todo-list">
+                    <ul className="todo-items">
+                        {Object.keys(this.props.items).map(uuid => (
+                            <ToDoItem key={`todo-item-${uuid}`} data={this.props.items[uuid]} />
+                        ))}
+                    </ul>
+                </div>)
+            ||
             <div className="alert alert-info">
-                This is where the ToDo items will be.
+                Start by adding a new item
       </div>
         )
     }
